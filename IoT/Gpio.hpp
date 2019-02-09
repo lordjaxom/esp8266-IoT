f01bdfca3456516c15a99ee3a92fe9b31a34eacb
Sascha Volkenandt <sascha@akv-soft.de>
2019-02-09 14:02:34 +0100

    dead code removed

--- IoT/Gpio.hpp
+++ IoT/Gpio.hpp
@@ -9,30 +9,4 @@
 std::function< bool() > gpioInput( uint8_t pin, bool pullUp = true );
 std::function< void( bool value ) > gpioOutput( uint8_t pin, bool invert = false );
 
-class GpioInput
-{
-public:
-    explicit GpioInput( uint8_t pin, bool pullUp = true ) noexcept;
-    GpioInput( GpioInput const& ) = delete;
-
-    bool operator()() const;
-
-private:
-    uint8_t pin_;
-    bool pullUp_;
-};
-
-class GpioOutput
-{
-public:
-    explicit GpioOutput( uint8_t pin, bool invert = false ) noexcept;
-    GpioOutput( GpioOutput const& ) = delete;
-
-    void operator()( bool value );
-
-private:
-    uint8_t pin_;
-    bool invert_;
-};
-
 #endif // ESP8266_IOT_GPIO_HPP
