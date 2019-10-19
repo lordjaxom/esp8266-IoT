d37d804dc863269bcf7cfe251f906809f7d10111
Sascha Volkenandt <sascha@akv-soft.de>
2019-10-19 23:44:23 +0200

    example for standalone command

--- Untitled.cpp
+++ Untitled.cpp
@@ -1,7 +1,9 @@
+#include "IoT/Command.hpp"
 #include "IoT/Debounce.hpp"
 #include "IoT/Device.hpp"
 #include "IoT/Gpio.hpp"
 #include "IoT/IoT.hpp"
+#include "IoT/Logger.hpp"
 #include "IoT/PushButton.hpp"
 #include "IoT/Remote.hpp"
 #include "IoT/SceneManager.hpp"
@@ -13,12 +15,14 @@ IoTClass IoT( "akvsoft", "sacomoco02047781", "192.168.178.28", 1883 );
 
 PushButton button( debounce( gpioInput( buttonPin )));
 Device output( "Untitled/Output", gpioOutput( outputPin ));
+Command standby( "Untitled", "STANDBY", [] { log( "STANDBY was triggered" ); } );
 
 SceneManager sceneManager( "Untitled" );
 
 void setup()
 {
     button.clickedEvent += []( unsigned clicked ) { sceneManager.sceneButtonClicked( clicked ); };
+    button.longClickedEvent += [] { standby.trigger(); };
 
     sceneManager.addSceneDevice( output );
 
